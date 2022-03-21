c24288488a956f1616c855a55a61e5ed10ab38b4
TechsavvYash <mittal.yash12@hotmail.com>
2022-03-21 23:41:59 +0530

    initial server setup
    Signed-off-by: TechsavvYash <mittal.yash12@hotmail.com>

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express() ;
+const cors = require('cors')
+const session = require("express-session")
+require("dotenv").config() ;
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors({
+    origin: process.env.CORS_URL,
+    credentials: true
+}));
+
+
+app.get('/', (req, res) => {
+    res.send("Hey, There!")
+})
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log("http://localhost:8080")
+})
